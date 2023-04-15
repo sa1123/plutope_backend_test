1dd627afca1485282f5b97724692ed6b2b42cf1e
Sahil <51375919+sa1123@users.noreply.github.com>
2023-04-16 00:20:17 +0530

    schema

---
+++ Customers.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const CustomerInfoSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    phone : {
+        type : Number,
+        required : true
+    },
+    size : {
+        type : String,
+        required : true,
+        enum : ['xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl']
+    },
+    id : {
+        type : Number,
+        required : true,
+        unique : true
+    },
+    waist : {
+        type : Number,
+        required : true,
+        enum : [26, 28, 30, 32, 34, 36, 38, 40, 42]
+    },
+    gender : {
+        type : String,
+        required : true,
+        enum : ['male', 'female', 'other']
+    }
+})
+
+const CustomerInfo = mongoose.model('customer', CustomerInfoSchema)
+
+module.exports = CustomerInfo;
