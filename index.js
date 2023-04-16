fc0227b1258df4a102652ed5b554f65672b599c8
Sahil <51375919+sa1123@users.noreply.github.com>
2023-04-16 11:33:02 +0530

    rearranged; final

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const db = require('./db')
-const movieRouter = require('./routes/movie-router')
+const customerRouter = require('./routes/customer-router')
 
 const app = express()
-const apiPort = 8000
+const apiPort = 8080
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -14,10 +14,6 @@ app.use(bodyParser.json())
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.use('/api', movieRouter)
+app.use('/api', customerRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
